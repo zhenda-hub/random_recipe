b022d72b59bee759b24a47995b383d341458fba6
zzdnb <1955093734@qq.com>
2022-12-04 02:54:23 +0800

    添加新的函数结构

--- main_gui.py
+++ main_gui.py
@@ -1,7 +1,8 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox, QMessageBox, QInputDialog, QLineEdit
 from PyQt5.QtGui import QIcon
-from ui.random_recipe import Ui_MainWindow
+# from ui.random_recipe import Ui_MainWindow
+from ui.random_recipe2 import Ui_MainWindow
 import sys
 from random import randrange
 from collections import Counter
@@ -15,14 +16,15 @@ class MyGui(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-        self.setWindowFlags(Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint)
-        self.setWindowIcon(QIcon('statics/food.ico'))
-        self.result = []
+        self.setWindowFlags(Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint)  # 最大化最小化设置
+        self.setWindowIcon(QIcon('statics/food.ico'))  # 图标设置
 
+        self.result = []
         self.pushButton_run.clicked.connect(self.run)
-        self.pushButton_all_select.clicked.connect(self.check_all)
-        self.pushButton_add.clicked.connect(self.add_recipe)
         self.pushButton_reset.clicked.connect(self.reset)
+        self.pushButton_add.clicked.connect(self.add_recipe)
+        self.pushButton_save.clicked.connect(self.save)
+        self.pushButton_load.clicked.connect(self.load)
 
     def run(self):
         recipe_list = []
@@ -43,24 +45,30 @@ class MyGui(QMainWindow, Ui_MainWindow):
         self.textBrowser.append('前三甲：')
         self.textBrowser.append(f'{counter.most_common(3)}')
 
-    def check_all(self):
-        all_check_list = self.groupBox.findChildren(QCheckBox)
-        flag = all_check_list[0].isChecked()
-        for item in all_check_list:
-            if flag:
-                item.setChecked(False)
-            else:
-                item.setChecked(True)
-
-    def add_recipe(self):
-        value, flag = QInputDialog.getText(self, "添加食谱", "请输入希望添加的食谱:", QLineEdit.Normal, "焖面")
-        if flag:
-            check_box = QCheckBox(value)
-            self.verticalLayout_3.addWidget(check_box)
-
     def reset(self):
         self.result = []
         self.textBrowser.setText('')
+
+    # def check_all(self):
+    #     all_check_list = self.groupBox.findChildren(QCheckBox)
+    #     flag = all_check_list[0].isChecked()
+    #     for item in all_check_list:
+    #         if flag:
+    #             item.setChecked(False)
+    #         else:
+    #             item.setChecked(True)
+
+    def add_recipe(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+    def _open_file(self):
+        pass
 
 
 if __name__ == '__main__':
